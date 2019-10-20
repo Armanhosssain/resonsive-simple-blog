848f1175f03d3f0cdb037802e5c236f190e6c7f5
Arman hossain <armanh688@gmail.com>
2019-10-21 00:43:55 +0600

    responsive simple blog

---
+++ simple-blog.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/uikit.min.css">
+  <link rel="shortcut icon" type="image/x-icon" href="img/a.png">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+	<title>simple blog</title>
+</head>
+<body>
+	<!-- start home get started -->
+  <section id="getstarted" class="py-5 text-center text-light">
+    <div class="dark-overlay">
+      <div class="container">
+        <div class="row">
+          <div class="col mt-5 pt-4">
+            <h3>Are you ready to get started</h3>
+            <p class="lead">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ut soluta consectetur ab aliquam distinctio excepturi, magnam assumenda iste cum!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end home get started -->
+   <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/uikit.min.js"></script>
+  <script src="js/uikit-icons.min.js"></script>
+</body>
+</html>
